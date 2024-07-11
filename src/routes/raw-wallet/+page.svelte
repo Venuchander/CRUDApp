7e75f6c6310998f93e359a10fb6ebd224238d875
Venuchander <venuchander1@gmail.com>
2024-07-11 22:40:51 +0530

    update

--- src/routes/raw-wallet/+page.svelte
+++ src/routes/raw-wallet/+page.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
-    import {PrivateKey} from '@bsv/sdk'
+  import { PrivateKey } from "@bsv/sdk";
 
-    let WIF:string
-    let address:string
+  let WIF: string;
+  let address: String;
 
-    function createKeys () {
+  function createKeys() {
+    const privKey = PrivateKey.fromRandom();
+    WIF = privKey.toWif();
+    console.log("Private Key WIF format is ", WIF);
+    address = privKey.toAddress();
+    console.log("Your bitcoin address is ", address);
+  }
+</script>
 
-        const privKey = PrivateKey.fromRandom()
-        WIF = privKey.toWif()
-        console.log("Private Key WIF format is ",WIF)
-        address = privKey.toAddress()
-        console.log("Your Bitcoin address is ",address)
-    }
-
-    </script>
-
-    <section>
-        <button class="bg-black text-white p-2 rounded mx-auto" on:click={createKeys}> Generate New Wallet Keys </button>
-        <p>Private Key WIF format is {WIF}</p>
-        <p>Your Bitcoin Address is {address}</p>
-    </section>
+<section>
+  <div class="p-10">
+    <button on:click={createKeys} class="bg-black text-white px-3 py-2 mb-2"
+      >Generate New Wallet Keys</button
+    >
+    <p>Private Key WIF format is: <span class="font-semibold">{WIF}</span></p>
+    <p>Your Bitcoit address is: <span class="font-semibold">{address}</span></p>
+  </div>
+</section>
